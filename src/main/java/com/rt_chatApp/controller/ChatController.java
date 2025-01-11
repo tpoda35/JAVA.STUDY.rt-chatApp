f6cc463f0577138876c47a877a5bc3339ed09a40
tpoda <tpoda35@gmail.com>
2025-01-11 23:06:15 +0100

    Implemented the chat system.

--- src/main/java/com/rt_chatApp/controller/ChatController.java
+++ src/main/java/com/rt_chatApp/controller/ChatController.java
@@ -1,7 +1,25 @@
 package com.rt_chatApp.controller;
 
+import com.rt_chatApp.Dto.ChatMsgDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/chat")
+    public void handleMessage(ChatMsgDto chatMessage) {
+        String destination = "/queue/chat/" + chatMessage.recipientId();
+        messagingTemplate.convertAndSend(destination, chatMessage);
+    }
+
 }
