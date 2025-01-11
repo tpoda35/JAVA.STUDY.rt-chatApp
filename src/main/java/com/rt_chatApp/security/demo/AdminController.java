5ea93ffd1bc42473b9d06840d4b07c5f4abcac23
tpoda <tpoda35@gmail.com>
2025-01-11 23:05:48 +0100

    Added @Hidden.

--- src/main/java/com/rt_chatApp/security/demo/AdminController.java
+++ src/main/java/com/rt_chatApp/security/demo/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
+@Hidden
 public class AdminController {
 
     @GetMapping
