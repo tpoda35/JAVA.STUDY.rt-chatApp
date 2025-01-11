e7b5737e7b3ee2e8823c45f0b7ad7cd524eca009
tpoda <tpoda35@gmail.com>
2025-01-11 23:06:44 +0100

    Added hidden.

--- src/main/java/com/rt_chatApp/security/demo/ManagementController.java
+++ src/main/java/com/rt_chatApp/security/demo/ManagementController.java
@@ -1,5 +1,6 @@
 package com.rt_chatApp.security.demo;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/management")
 @Tag(name = "Management")
+@Hidden
 public class ManagementController {
 
 
