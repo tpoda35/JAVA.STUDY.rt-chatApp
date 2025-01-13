b9d581a9650f4d2d6ebb2b2eeacbb65c93ebcf5a
tpoda <tpoda35@gmail.com>
2025-01-13 20:41:06 +0100

    Removed some unnecessary lines.

--- src/main/java/com/rt_chatApp/security/config/SecurityConfiguration.java
+++ src/main/java/com/rt_chatApp/security/config/SecurityConfiguration.java
@@ -72,8 +72,7 @@ public class SecurityConfiguration {
                         logout.logoutUrl("/api/v1/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-                )
-        ;
+                );
 
         return http.build();
     }
