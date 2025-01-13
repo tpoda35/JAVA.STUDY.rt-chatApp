77a6d15a2d7cab3ccc0afb18a357e7ec0e913171
tpoda <tpoda35@gmail.com>
2025-01-13 20:40:07 +0100

    Implemented disconnect and findOnlineUsers (This later will be async).

--- src/main/java/com/rt_chatApp/controller/ChatController.java
+++ src/main/java/com/rt_chatApp/controller/ChatController.java
@@ -1,25 +1,38 @@
 package com.rt_chatApp.controller;
 
 import com.rt_chatApp.Dto.ChatMsgDto;
+import com.rt_chatApp.security.user.User;
+import com.rt_chatApp.services.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatService service;
 
-    @Autowired
-    public ChatController(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/topic")
+    public User disconnect(
+            @Payload User user
+    ) {
+        service.disconnect(user);
+        return user;
     }
 
-    @MessageMapping("/chat")
-    public void handleMessage(ChatMsgDto chatMessage) {
-        String destination = "/queue/chat/" + chatMessage.recipientId();
-        messagingTemplate.convertAndSend(destination, chatMessage);
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findConnectedUsers() {
+        return ResponseEntity.ok(service.findConnectedUsers());
     }
 
 }
