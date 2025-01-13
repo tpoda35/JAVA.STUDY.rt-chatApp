92b72168b3823bd2a5ca861d25d7fa5363c2a2ea
tpoda <tpoda35@gmail.com>
2025-01-13 20:41:24 +0100

    Added disconnect and findAllByStatus.

---
+++ src/main/java/com/rt_chatApp/services/ChatService.java
@@ -0,0 +1,30 @@
+package com.rt_chatApp.services;
+
+import com.rt_chatApp.security.user.Status;
+import com.rt_chatApp.security.user.User;
+import com.rt_chatApp.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final UserRepository repository;
+
+    public void disconnect(User user){
+        var storedUser = repository.findById(user.getId())
+                .orElse(null);
+        if (storedUser != null){
+            storedUser.setStatus(Status.OFFLINE);
+            repository.save(storedUser);
+        }
+    }
+
+    public List<User> findConnectedUsers(){
+        return repository.findAllByStatus(Status.ONLINE);
+    }
+
+}
