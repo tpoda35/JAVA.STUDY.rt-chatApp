82a40be1590cc9df2043771706874c95672ac1e1
tpoda <tpoda35@gmail.com>
2025-01-14 18:01:53 +0100

    Added controller for chat.

--- src/main/java/com/rt_chatApp/controller/ChatController.java
+++ src/main/java/com/rt_chatApp/controller/ChatController.java
@@ -1,17 +1,16 @@
 package com.rt_chatApp.controller;
 
-import com.rt_chatApp.Dto.ChatMsgDto;
-import com.rt_chatApp.security.user.User;
-import com.rt_chatApp.services.ChatService;
+import com.rt_chatApp.Dto.ChatMessage;
+import com.rt_chatApp.Dto.ChatNotification;
+import com.rt_chatApp.services.ChatMessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,20 +18,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final ChatService service;
+    private final SimpMessagingTemplate template;
+    private final ChatMessageService service;
 
-    @MessageMapping("/user.disconnectUser")
-    @SendTo("/user/topic")
-    public User disconnect(
-            @Payload User user
+    @MessageMapping("/chat")
+    public void processMessage(
+            @Payload ChatMessage chatMessage
     ) {
-        service.disconnect(user);
-        return user;
+        ChatMessage savedMsg = service.save(chatMessage);
+        template.convertAndSendToUser(
+                String.valueOf(chatMessage.getRecipientId()), "/queue/messages",
+                ChatNotification.builder()
+                        .id(savedMsg.getId())
+                        .senderId(savedMsg.getSenderId())
+                        .recipientId(savedMsg.getRecipientId())
+                        .content(savedMsg.getContent())
+                        .build()
+        );
     }
 
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> findConnectedUsers() {
-        return ResponseEntity.ok(service.findConnectedUsers());
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<ChatMessage>> findChatMessages(
+             @PathVariable("senderId") Integer senderId,
+             @PathVariable("recipientId") Integer recipientId
+    ) {
+        return ResponseEntity.ok(service.findChatMessages(senderId, recipientId));
     }
 
 }
