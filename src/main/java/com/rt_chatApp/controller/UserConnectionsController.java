95ed9a15c658ae8d9308850739596aebceb8dee2
tpoda <tpoda35@gmail.com>
2025-01-14 17:59:46 +0100

    File renamed.

---
+++ src/main/java/com/rt_chatApp/controller/UserConnectionsController.java
@@ -0,0 +1,35 @@
+package com.rt_chatApp.controller;
+
+import com.rt_chatApp.security.user.User;
+import com.rt_chatApp.services.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserConnectionsController {
+
+    private final ChatService service;
+
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/topic")
+    public User disconnect(
+            @Payload User user
+    ) {
+        service.disconnect(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findConnectedUsers() {
+        return ResponseEntity.ok(service.findConnectedUsers());
+    }
+
+}
