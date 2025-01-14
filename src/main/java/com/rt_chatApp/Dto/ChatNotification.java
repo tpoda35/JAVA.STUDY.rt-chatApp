10e9d4638599364fa3f32f4c758fb407cf9f14c5
tpoda <tpoda35@gmail.com>
2025-01-14 18:01:06 +0100

    Added notification object.

---
+++ src/main/java/com/rt_chatApp/Dto/ChatNotification.java
@@ -0,0 +1,19 @@
+package com.rt_chatApp.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatNotification {
+
+    private String id;
+    private Integer senderId;
+    private Integer recipientId;
+    private String content;
+
+}
