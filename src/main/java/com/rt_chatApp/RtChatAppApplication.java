fcf408c8492c5d558f7bcfa2edb5c1de2d9f864f
tpoda <tpoda35@gmail.com>
2025-01-14 18:00:21 +0100

    Commented out the account seeding.

--- src/main/java/com/rt_chatApp/RtChatAppApplication.java
+++ src/main/java/com/rt_chatApp/RtChatAppApplication.java
@@ -19,32 +19,32 @@ public class RtChatAppApplication {
 		SpringApplication.run(RtChatAppApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			AuthenticationService service
-	) {
-		if (!service.userExists("admin@mail.com") && !service.userExists("manager@mail.com")) {
-			return args -> {
-				var admin = RegisterRequest.builder()
-						.firstname("Admin")
-						.lastname("Admin")
-						.email("admin@mail.com")
-						.password("password")
-						.role(ADMIN)
-						.build();
-				System.out.println("Admin token: " + service.register(admin).getAccessToken());
-
-				var manager = RegisterRequest.builder()
-						.firstname("Admin")
-						.lastname("Admin")
-						.email("manager@mail.com")
-						.password("password")
-						.role(MANAGER)
-						.build();
-				System.out.println("Manager token: " + service.register(manager).getAccessToken());
-			};
-		}
-
-		return null;
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			AuthenticationService service
+//	) {
+//		if (!service.userExists("admin@mail.com") && !service.userExists("manager@mail.com")) {
+//			return args -> {
+//				var admin = RegisterRequest.builder()
+//						.firstname("Admin")
+//						.lastname("Admin")
+//						.email("admin@mail.com")
+//						.password("password")
+//						.role(ADMIN)
+//						.build();
+//				System.out.println("Admin token: " + service.register(admin).getAccessToken());
+//
+//				var manager = RegisterRequest.builder()
+//						.firstname("Admin")
+//						.lastname("Admin")
+//						.email("manager@mail.com")
+//						.password("password")
+//						.role(MANAGER)
+//						.build();
+//				System.out.println("Manager token: " + service.register(manager).getAccessToken());
+//			};
+//		}
+//
+//		return null;
+//	}
 }
