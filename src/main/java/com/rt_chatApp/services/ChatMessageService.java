7f247f29a61f1729e8a85b74decd03aea608fe72
tpoda <tpoda35@gmail.com>
2025-01-14 18:01:26 +0100

    Added service for chat messages.

---
+++ src/main/java/com/rt_chatApp/services/ChatMessageService.java
@@ -0,0 +1,36 @@
+package com.rt_chatApp.services;
+
+import com.rt_chatApp.Dto.ChatMessage;
+import com.rt_chatApp.repository.ChatMessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final ChatMessageRepository repository;
+    private final ChatRoomService service;
+
+    public ChatMessage save(ChatMessage chatMessage){
+        var chatId = service.getChatRoomId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true)
+                .orElseThrow(); // Will be implemented.
+
+        chatMessage.setChatId(chatId);
+        return repository.save(chatMessage);
+    }
+
+    public List<ChatMessage> findChatMessages(
+            Integer senderId, Integer recipientId
+    ) {
+        var chatId = service.getChatRoomId(senderId, recipientId, false);
+
+        return chatId.map(repository::findByChatId)
+                .orElse(new ArrayList<>());
+    }
+
+
+}
