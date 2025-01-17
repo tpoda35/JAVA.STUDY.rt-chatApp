82f118bea60071dbeb6aa831da577f5f99adb54d
tpoda <tpoda35@gmail.com>
2025-01-17 15:34:39 +0100

    Added OAuth2 authentication logic.

---
+++ src/main/java/com/rt_chatApp/security/oAuth2/CustomOAuth2UserService.java
@@ -0,0 +1,38 @@
+package com.rt_chatApp.security.oAuth2;
+
+import com.rt_chatApp.security.user.AuthProvider;
+import com.rt_chatApp.security.user.User;
+import com.rt_chatApp.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository repository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        User user = repository.findByEmail(email)
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setEmail(email);
+                    newUser.setUsername(name);
+                    newUser.setAuthProvider(AuthProvider.GOOGLE);
+                    return repository.save(newUser);
+                });
+
+        return new DefaultOAuth2User(oAuth2User.getAuthorities(), oAuth2User.getAttributes(), "email");
+    }
+}
---
+++ src/main/java/com/rt_chatApp/security/oAuth2/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,50 @@
+package com.rt_chatApp.security.oAuth2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rt_chatApp.security.auth.AuthenticationResponse;
+import com.rt_chatApp.security.config.JwtService;
+import com.rt_chatApp.security.user.User;
+import com.rt_chatApp.security.user.UserRepository;
+import com.sun.security.auth.UserPrincipal;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final UserRepository repository;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication)
+            throws IOException, ServletException {
+
+        OAuth2User userPrincipal = (OAuth2User) authentication.getPrincipal();
+        User user = repository.findByEmail(userPrincipal.getName())
+                .orElseThrow(() -> new EntityNotFoundException("User not found."));
+
+        String jwt = jwtService.generateToken(user);
+        String refresh = jwtService.generateRefreshToken(user);
+
+        AuthenticationResponse authResponse = AuthenticationResponse.builder()
+                .accessToken(jwt)
+                .refreshToken(refresh)
+                .build();
+
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(authResponse));
+    }
+}
