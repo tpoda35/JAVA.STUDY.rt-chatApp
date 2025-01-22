119be438fecff3ba4f52371ed97ebe2377ef7113
tpoda <tpoda35@gmail.com>
2025-01-22 15:35:46 +0100

    Modified the filter to access the cookies much more flexible way.

--- src/main/java/com/rt_chatApp/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/rt_chatApp/security/config/JwtAuthenticationFilter.java
@@ -52,11 +52,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       final String authHeader = request.getHeader("Authorization");
       final String jwt;
       final String userEmail;
-      if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+      if (authHeader == null) {
         filterChain.doFilter(request, response);
         return;
       }
-      jwt = authHeader.substring(7);
+
+      if (authHeader.startsWith("Bearer ")){
+        jwt = authHeader.substring(7);
+      } else {
+        jwt = authHeader;
+      }
+
       userEmail = jwtService.extractUsername(jwt);
       if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
         UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
