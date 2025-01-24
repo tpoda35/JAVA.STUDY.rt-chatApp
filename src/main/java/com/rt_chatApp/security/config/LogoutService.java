d7c724e97fd494e1e5161b31c3765a84b8077806
tpoda <tpoda35@gmail.com>
2025-01-24 15:25:52 +0100

    Recoded the logout logic. Now handles exceptions and cookies also.

--- src/main/java/com/rt_chatApp/security/config/LogoutService.java
+++ src/main/java/com/rt_chatApp/security/config/LogoutService.java
@@ -1,39 +1,78 @@
 package com.rt_chatApp.security.config;
 
+import com.rt_chatApp.Exceptions.LogoutException;
 import com.rt_chatApp.security.token.TokenRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Service
-@RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
 
   private final TokenRepository tokenRepository;
+  private final HandlerExceptionResolver resolver;
+
+    public LogoutService(
+            TokenRepository tokenRepository,
+            @Qualifier("handlerExceptionResolver")
+            HandlerExceptionResolver resolver) {
+        this.tokenRepository = tokenRepository;
+        this.resolver = resolver;
+    }
 
   @Override
+  @Transactional
   public void logout(
-      HttpServletRequest request,
-      HttpServletResponse response,
-      Authentication authentication
+          HttpServletRequest request,
+          HttpServletResponse response,
+          Authentication authentication
   ) {
-    final String authHeader = request.getHeader("Authorization");
-    final String jwt;
-    if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
-      return;
+      try {
+        String jwt = null;
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+          for (Cookie cookie : cookies) {
+            if ("Authorization".equals(cookie.getName())) {
+              jwt = cookie.getValue();
+              break;
+            }
+          }
+        }
+
+        if (jwt == null) {
+          SecurityContextHolder.clearContext();
+          throw new EntityNotFoundException("Missing JWT from header.");
+        }
+
+        var storedToken = tokenRepository.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+          storedToken.setExpired(true);
+          storedToken.setRevoked(true);
+          tokenRepository.save(storedToken);
+
+          for (Cookie cookie : cookies){
+            cookie.setMaxAge(0);
+            cookie.setValue(null);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+          }
+
+          SecurityContextHolder.clearContext();
+        } else {
+          throw new LogoutException("Logout failed.");
+        }
+      } catch (Exception e){
+        resolver.resolveException(request,response, null, e);
+      }
     }
-    jwt = authHeader.substring(7);
-    var storedToken = tokenRepository.findByToken(jwt)
-        .orElse(null);
-    if (storedToken != null) {
-      storedToken.setExpired(true);
-      storedToken.setRevoked(true);
-      tokenRepository.save(storedToken);
-      SecurityContextHolder.clearContext();
-    }
-  }
 }
