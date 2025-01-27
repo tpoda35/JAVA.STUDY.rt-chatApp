726b5cb8afbafd8a0120d1d562a34aff02b82151
tpoda <tpoda35@gmail.com>
2025-01-27 17:18:15 +0100

    Added restricted access for authenticated users.

---
+++ src/main/java/com/rt_chatApp/security/config/RestrictAccessForLoggedInUsersFilter.java
@@ -0,0 +1,38 @@
+package com.rt_chatApp.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RestrictAccessForLoggedInUsersFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String uri = request.getRequestURI();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (
+                authentication != null &&
+                authentication.isAuthenticated() &&
+                !"anonymousUser".equals(authentication.getPrincipal()) &&
+                ("/login.html".equals(uri) || "/register.html".equals(uri))
+        ) {
+            response.sendRedirect("/index.html");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
