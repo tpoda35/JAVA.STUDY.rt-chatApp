45fb49003fe367fd487d8044aae0fc7b39f9ff08
tpoda <tpoda35@gmail.com>
2025-01-30 13:41:01 +0100

    Added UserInfoDto.

---
+++ src/main/java/com/rt_chatApp/Dto/UserInfoDto.java
@@ -0,0 +1,14 @@
+package com.rt_chatApp.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoDto {
+    private String email;
+}
