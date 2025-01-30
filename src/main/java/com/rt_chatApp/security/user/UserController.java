4ac0323fab989417f3192101f5de987d3334613b
tpoda <tpoda35@gmail.com>
2025-01-30 13:40:25 +0100

    Added getUserInfo.

--- src/main/java/com/rt_chatApp/security/user/UserController.java
+++ src/main/java/com/rt_chatApp/security/user/UserController.java
@@ -1,11 +1,10 @@
 package com.rt_chatApp.security.user;
 
+import com.rt_chatApp.Dto.UserInfoDto;
+import com.rt_chatApp.security.config.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -15,6 +14,7 @@ import java.security.Principal;
 public class UserController {
 
     private final UserService service;
+    private final JwtService jwtService;
 
     @PatchMapping
     public ResponseEntity<?> changePassword(
@@ -23,5 +23,19 @@ public class UserController {
     ) {
         service.changePassword(request, connectedUser);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/getUserInfo")
+    public ResponseEntity<?> getUserInfo(
+            @CookieValue("Authorization") String token
+    ){
+        String email = jwtService.extractUsername(token);
+        if (email == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(UserInfoDto.builder()
+                .email(email)
+                .build());
     }
 }
