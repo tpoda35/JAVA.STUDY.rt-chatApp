ac25d1168aeb92179e6761848f7ca40082bbcc54
tpoda <tpoda35@gmail.com>
2025-02-01 14:54:44 +0100

    Added validation.

--- src/main/java/com/rt_chatApp/security/auth/RegisterRequest.java
+++ src/main/java/com/rt_chatApp/security/auth/RegisterRequest.java
@@ -1,10 +1,13 @@
 package com.rt_chatApp.security.auth;
 
-import com.rt_chatApp.security.user.Role;
+import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 
 @Data
 @Builder
@@ -12,9 +15,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+  @NotBlank(message = "Firstname field cannot be blank.")
   private String firstname;
+  @NotBlank(message = "Lastname field cannot be blank.")
   private String lastname;
+  @NotBlank(message = "Email field cannot be blank.")
+  @Email(message = "Invalid Email format.")
   private String email;
   private String password;
-  private Role role;
+  private String confirmPassword;
+
+  @PrePersist
+  protected void checkPasswords(){
+    if (!password.equals(confirmPassword)){
+      throw new BadCredentialsException("Passwords does not match.");
+    }
+  }
 }
