e831b297e0acdfc176cc9c687c45f726d51b7c64
tpoda <tpoda35@gmail.com>
2025-02-04 13:43:13 +0100

    Added Controller and Service for thymeleaf.

---
+++ src/main/java/com/rt_chatApp/Thymeleaf/Controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.rt_chatApp.Thymeleaf.Controller;
+
+import com.rt_chatApp.Thymeleaf.Service.HomeService;
+import com.rt_chatApp.security.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final HomeService service;
+
+    @GetMapping("/chat")
+    public String viewChatPage(Model model){
+        model.addAttribute("fName", service.getAuthenticatedUserName());
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/rt_chatApp/Thymeleaf/Service/HomeService.java
@@ -0,0 +1,21 @@
+package com.rt_chatApp.Thymeleaf.Service;
+
+import com.rt_chatApp.security.user.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public String getAuthenticatedUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof User user){
+            return user.getFirstname();
+        }
+
+        return null;
+    }
+
+}
