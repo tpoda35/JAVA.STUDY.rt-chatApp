38dd6e4e42e87d255a7cc35f4e8e23faf2e5ac45
tpoda <tpoda35@gmail.com>
2025-02-07 16:14:25 +0100

    Added the firstName to the model.

--- src/main/java/com/rt_chatApp/Thymeleaf/Controller/HomeController.java
+++ src/main/java/com/rt_chatApp/Thymeleaf/Controller/HomeController.java
@@ -2,6 +2,7 @@ package com.rt_chatApp.Thymeleaf.Controller;
 
 import com.rt_chatApp.Thymeleaf.Service.HomeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,16 @@ public class HomeController {
 
     private final HomeService service;
 
-    @GetMapping("/chat")
-    public String viewChatPage(Model model){
-        model.addAttribute("fName", service.getAuthenticatedUserName());
-        return "index";
+    @GetMapping("/")
+    public String redirectToChat(
+            Model model,
+            Authentication authentication
+    ) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            model.addAttribute("fName", service.getAuthenticatedUserName());
+            return "index";
+        }
+        return "login.html";
     }
 
 }
