f1db7a956ac14cd703a6bdcc367bfce5eaeda031
tpoda35 <tpoda35@gmail.com>
2025-02-10 19:10:11 +0100

    Added more data to user creation.

--- src/main/java/com/rt_chatApp/security/oAuth2/CustomOAuth2UserService.java
+++ src/main/java/com/rt_chatApp/security/oAuth2/CustomOAuth2UserService.java
@@ -1,9 +1,6 @@
 package com.rt_chatApp.security.oAuth2;
 
-import com.rt_chatApp.security.user.AuthProvider;
-import com.rt_chatApp.security.user.Role;
-import com.rt_chatApp.security.user.User;
-import com.rt_chatApp.security.user.UserRepository;
+import com.rt_chatApp.security.user.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -11,6 +8,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import static com.rt_chatApp.security.user.Status.ONLINE;
 
 @Service
 @RequiredArgsConstructor
@@ -23,13 +22,16 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String email = oAuth2User.getAttribute("email");
+        String fName = oAuth2User.getAttribute("given_name");
 
         repository.findByEmail(email)
                 .orElseGet(() -> {
                     User newUser = new User();
                     newUser.setEmail(email);
+                    newUser.setFirstname(fName);
                     newUser.setAuthProvider(AuthProvider.GOOGLE);
                     newUser.setRole(Role.USER);
+                    newUser.setStatus(ONLINE);
                     return repository.save(newUser);
                 });
 
