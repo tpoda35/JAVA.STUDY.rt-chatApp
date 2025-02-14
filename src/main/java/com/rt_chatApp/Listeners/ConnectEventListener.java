23297a87c641a9a7439333b4ce444ac47cc80b42
tpoda <tpoda35@gmail.com>
2025-02-14 15:11:11 +0100

    Added Online/Offline status system when user connects or disconnects.

---
+++ src/main/java/com/rt_chatApp/Listeners/ConnectEventListener.java
@@ -0,0 +1,31 @@
+package com.rt_chatApp.Listeners;
+
+import com.rt_chatApp.security.user.User;
+import com.rt_chatApp.security.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+
+import java.security.Principal;
+
+@Component
+@RequiredArgsConstructor
+public class ConnectEventListener implements ApplicationListener<SessionConnectEvent> {
+
+    private final UserService userService;
+
+    @Override
+    public void onApplicationEvent(SessionConnectEvent event) {
+        Principal principal = event.getUser();
+
+        if (principal instanceof UsernamePasswordAuthenticationToken authenticationToken) {
+            Object principalObj = authenticationToken.getPrincipal();
+
+            if (principalObj instanceof User user) {
+                userService.connect(user.getId());
+            }
+        }
+    }
+}
