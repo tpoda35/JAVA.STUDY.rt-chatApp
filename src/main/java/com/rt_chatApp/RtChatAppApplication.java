3aa7f3aace74c4b091485a5bb4ac3b0dd18e4c24
tpoda <tpoda35@gmail.com>
2025-02-15 21:52:48 +0100

    Removed the seeding.

--- src/main/java/com/rt_chatApp/RtChatAppApplication.java
+++ src/main/java/com/rt_chatApp/RtChatAppApplication.java
@@ -1,16 +1,9 @@
 package com.rt_chatApp;
 
-import com.rt_chatApp.security.auth.AuthenticationService;
-import com.rt_chatApp.security.auth.RegisterRequest;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import static com.rt_chatApp.security.user.Role.ADMIN;
-import static com.rt_chatApp.security.user.Role.MANAGER;
 
 @SpringBootApplication
 @EnableAsync
@@ -20,32 +13,4 @@ public class RtChatAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RtChatAppApplication.class, args);
 	}
-
-//	@Bean
-//	public CommandLineRunner commandLineRunner(
-//			AuthenticationService service
-//	) {
-//		if (!service.userExists("admin@mail.com") && !service.userExists("manager@mail.com")) {
-//			return args -> {
-//				var admin = RegisterRequest.builder()
-//						.firstname("Admin")
-//						.lastname("Admin")
-//						.email("admin@mail.com")
-//						.password("password")
-//						.role(ADMIN)
-//						.build();
-//				System.out.println("Admin token: " + service.register(admin).getAccessToken());
-//
-//				var manager = RegisterRequest.builder()
-//						.firstname("Admin")
-//						.lastname("Admin")
-//						.email("manager@mail.com")
-//						.password("password")
-//						.role(MANAGER)
-//						.build();
-//				System.out.println("Manager token: " + service.register(manager).getAccessToken());
-//			};
-//		}
-//		return null;
-//	}
 }
