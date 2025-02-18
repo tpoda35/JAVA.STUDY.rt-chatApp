356f2f1a5ddfd1286a2ec620df759c29b544c3c0
tpoda35 <tpoda35@gmail.com>
2025-02-18 23:50:03 +0100

    Changed user creation to builder.

--- src/main/java/com/rt_chatApp/security/oAuth2/CustomOAuth2UserService.java
+++ src/main/java/com/rt_chatApp/security/oAuth2/CustomOAuth2UserService.java
@@ -1,7 +1,5 @@
 package com.rt_chatApp.security.oAuth2;
 
-import com.rt_chatApp.security.user.AuthProvider;
-import com.rt_chatApp.security.user.Role;
 import com.rt_chatApp.security.user.User;
 import com.rt_chatApp.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +10,8 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import static com.rt_chatApp.security.user.AuthProvider.GOOGLE;
+import static com.rt_chatApp.security.user.Role.USER;
 import static com.rt_chatApp.security.user.Status.OFFLINE;
 
 @Service
@@ -25,16 +25,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String email = oAuth2User.getAttribute("email");
-        String fName = oAuth2User.getAttribute("given_name");
+        String displayName = oAuth2User.getAttribute("given_name");
 
         repository.findByEmail(email)
                 .orElseGet(() -> {
-                    User newUser = new User();
-                    newUser.setEmail(email);
-                    newUser.setFirstname(fName);
-                    newUser.setAuthProvider(AuthProvider.GOOGLE);
-                    newUser.setRole(Role.USER);
-                    newUser.setStatus(OFFLINE);
+                    User newUser = User.builder()
+                            .email(email)
+                            .displayName(displayName)
+                            .authProvider(GOOGLE)
+                            .role(USER)
+                            .status(OFFLINE)
+                            .build();
+
                     return repository.save(newUser);
                 });
 
