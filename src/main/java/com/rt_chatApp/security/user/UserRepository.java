c8039936a3c4140ed1c9f8129533f2b5c1fc05e4
tpoda35 <tpoda35@gmail.com>
2025-02-18 23:54:33 +0100

    Removed unused method.

--- src/main/java/com/rt_chatApp/security/user/UserRepository.java
+++ src/main/java/com/rt_chatApp/security/user/UserRepository.java
@@ -1,12 +1,11 @@
 package com.rt_chatApp.security.user;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
-  List<User> findAllByStatus(Status status);
   boolean existsByEmail(String email);
 }
