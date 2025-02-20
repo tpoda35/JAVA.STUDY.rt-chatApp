e4762c8f978731cfb138858065bed286f3b72a29
tpoda <tpoda35@gmail.com>
2025-02-20 20:57:11 +0100

    Added redirections.

--- src/main/java/com/rt_chatApp/Thymeleaf/Controller/HomeController.java
+++ src/main/java/com/rt_chatApp/Thymeleaf/Controller/HomeController.java
@@ -14,15 +14,51 @@ public class HomeController {
     private final HomeService service;
 
     @GetMapping("/")
-    public String redirectToChat(
+    public String redirectToIndex(
             Model model,
             Authentication authentication
     ) {
-        if (authentication != null && authentication.isAuthenticated()) {
+        if (isAuthenticated(authentication)) {
             model.addAttribute("dName", service.getAuthenticatedUserName());
             return "index";
         }
         return "redirect:/login";
     }
 
+    @GetMapping("/groups")
+    public String redirectToGroups(
+            Model model,
+            Authentication authentication
+    ) {
+        if (isAuthenticated(authentication)) {
+            return "groups";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/friends")
+    public String redirectToFriends(
+            Model model,
+            Authentication authentication
+    ) {
+        if (isAuthenticated(authentication)) {
+            return "friends";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/settings")
+    public String redirectToSettings(
+            Model model,
+            Authentication authentication
+    ) {
+        if (isAuthenticated(authentication)) {
+            return "settings";
+        }
+        return "redirect:/login";
+    }
+
+    private Boolean isAuthenticated(Authentication authentication) {
+        return authentication != null && authentication.isAuthenticated();
+    }
 }
