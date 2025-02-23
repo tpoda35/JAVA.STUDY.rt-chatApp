52c3732395ef66ef3be3d58860c6bd486f337804
tpoda <tpoda35@gmail.com>
2025-02-23 14:24:11 +0100

    Added SecurityContextHolder strategy.

--- src/main/java/com/rt_chatApp/RtChatAppApplication.java
+++ src/main/java/com/rt_chatApp/RtChatAppApplication.java
@@ -3,13 +3,14 @@ package com.rt_chatApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class RtChatAppApplication {
 
 	public static void main(String[] args) {
+		SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 		SpringApplication.run(RtChatAppApplication.class, args);
 	}
 }
