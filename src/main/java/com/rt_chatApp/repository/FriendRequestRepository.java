d55e323f429526b9891fb2653ed727ecc0a6164d
tpoda <tpoda35@gmail.com>
2025-02-23 20:45:32 +0100

    Added query for a method because of N+1 problem.

--- src/main/java/com/rt_chatApp/repository/FriendRequestRepository.java
+++ src/main/java/com/rt_chatApp/repository/FriendRequestRepository.java
@@ -3,6 +3,8 @@ package com.rt_chatApp.repository;
 import com.rt_chatApp.Model.FriendRequest;
 import com.rt_chatApp.security.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +16,7 @@ public interface FriendRequestRepository extends JpaRepository<FriendRequest, In
     boolean existsBySenderAndReceiver(User sender, User receiver);
 
     List<FriendRequest> findAllByReceiverId(Integer receiverId);
+
+    @Query("SELECT fr FROM FriendRequest fr JOIN FETCH fr.sender JOIN FETCH fr.receiver WHERE fr.id = :id")
+    Optional<FriendRequest> findByIdWithUsers(@Param("id") Integer id);
 }
