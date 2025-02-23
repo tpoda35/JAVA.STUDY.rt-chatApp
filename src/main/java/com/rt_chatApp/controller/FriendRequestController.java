95cff9c2d769fc02dfaefbdd5fcbdad672ab203c
tpoda <tpoda35@gmail.com>
2025-02-23 20:44:58 +0100

    Implemented the FriendRequestSystem fully.

--- src/main/java/com/rt_chatApp/controller/FriendRequestController.java
+++ src/main/java/com/rt_chatApp/controller/FriendRequestController.java
@@ -1,14 +1,12 @@
 package com.rt_chatApp.controller;
 
 import com.rt_chatApp.Dto.FriendRequestDto;
-import com.rt_chatApp.security.user.User;
 import com.rt_chatApp.security.user.UserService;
 import com.rt_chatApp.services.FriendRequestService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,32 +28,29 @@ public class FriendRequestController {
         return friendRequestService.sendFriendRequest(receiverUniqueName,  userService.getUser().getId());
     }
 
-    @GetMapping("/acceptRequest")
+    @PostMapping("/acceptRequest/{requestId}")
     public CompletableFuture<ResponseEntity<Void>> acceptRequest(
-            @RequestParam Integer requestId
+            @PathVariable("requestId") Integer requestId
     ) {
-
+        friendRequestService.acceptRequest(requestId);
         return CompletableFuture.completedFuture(
                 ResponseEntity.ok().build()
         );
     }
 
-    @PostMapping("/declineRequest")
-    public CompletableFuture<ResponseEntity<Void>> declineRequest(
-            @RequestParam Integer requestId
+    @PostMapping("/rejectRequest/{requestId}")
+    public CompletableFuture<ResponseEntity<Void>> rejectRequest(
+            @PathVariable("requestId") Integer requestId
     ){
+        friendRequestService.rejectRequest(requestId);
         return CompletableFuture.completedFuture(
                 ResponseEntity.ok().build()
         );
     }
 
     @GetMapping("/getReceivedRequests")
-    public CompletableFuture<List<FriendRequestDto>> getReceivedRequests(
-            Authentication authentication
-    ){
-        User user = (User) authentication.getPrincipal();
-        return null;
-//        return friendRequestService.getReceivedRequests(user.getId());
+    public CompletableFuture<List<FriendRequestDto>> getReceivedRequests(){
+        return friendRequestService.getReceivedRequests(userService.getUser().getId());
     }
 
 }
