2261253479a27d9a283e1ae94b2932e8f3e5210f
tpoda <tpoda35@gmail.com>
2025-02-24 18:07:58 +0100

    Added type and custom constructor.

--- src/main/java/com/rt_chatApp/Dto/FriendDto.java
+++ src/main/java/com/rt_chatApp/Dto/FriendDto.java
@@ -1,6 +1,9 @@
 package com.rt_chatApp.Dto;
 
+import com.rt_chatApp.Enum.FriendDtoType;
 import com.rt_chatApp.security.user.Status;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class FriendDto {
+
+    public FriendDto(Integer userId, String displayName, Status status) {
+        this.userId = userId;
+        this.displayName = displayName;
+        this.status = status;
+    }
+
+    @Enumerated(EnumType.STRING)
+    private FriendDtoType type;
     private Integer userId;
     private String displayName;
     private Status status;
