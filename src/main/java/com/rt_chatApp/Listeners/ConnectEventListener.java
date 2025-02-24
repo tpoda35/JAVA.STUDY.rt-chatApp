216d97afd0846ffee448f7ab5810266a9b6edb4e
tpoda <tpoda35@gmail.com>
2025-02-24 16:16:02 +0100

    Modified statuservice for the new friend system.

--- src/main/java/com/rt_chatApp/Listeners/ConnectEventListener.java
+++ src/main/java/com/rt_chatApp/Listeners/ConnectEventListener.java
@@ -1,26 +1,20 @@
 package com.rt_chatApp.Listeners;
 
 import com.rt_chatApp.security.user.User;
-import com.rt_chatApp.security.user.UserService;
 import com.rt_chatApp.services.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 
 import java.security.Principal;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
 public class ConnectEventListener implements ApplicationListener<SessionConnectEvent> {
 
-    private final UserService userService;
     private final UserStatusService statusService;
-    private final SimpMessagingTemplate messagingTemplate;
 
     @Override
     public void onApplicationEvent(SessionConnectEvent event) {
@@ -30,12 +24,8 @@ public class ConnectEventListener implements ApplicationListener<SessionConnectE
             Object principalObj = authenticationToken.getPrincipal();
 
             if (principalObj instanceof User user) {
-                statusService.addUser(user.getId());
-                userService.connect(user.getId());
-
-                CompletableFuture.delayedExecutor(500, TimeUnit.MILLISECONDS).execute(() -> {
-                    messagingTemplate.convertAndSend("/topic/onlineUsers", statusService.getOnlineUsers());
-                });
+                statusService.userOnline(user.getId())
+                        .thenCompose(aVoid -> statusService.notifyFriends(user.getId()));
             }
         }
     }
