57d759e5a1a4a6c2a05111896162938e9e70c0eb
tpoda <tpoda35@gmail.com>
2025-02-24 16:18:03 +0100

    Added query for getting friends.

--- src/main/java/com/rt_chatApp/security/user/UserRepository.java
+++ src/main/java/com/rt_chatApp/security/user/UserRepository.java
@@ -1,8 +1,12 @@
 package com.rt_chatApp.security.user;
 
+import com.rt_chatApp.Dto.FriendDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
   Optional<User> findByEmail(String email);
   boolean existsByEmail(String email);
   Optional<User> findByUniqueIdentifier(String uniqueIdentifier);
+
+  @Query("SELECT new com.rt_chatApp.Dto.FriendDto(u.id, u.displayName, u.status) " +
+          "FROM User u JOIN u.friends f WHERE u.id = :userId")
+  List<FriendDto> findFriendsByUserId(@Param("userId") Integer userId);
 }
