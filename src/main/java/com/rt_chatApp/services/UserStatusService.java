5b9d0c7c888c0b0976b2fdf12a07f04e802fd7e9
tpoda <tpoda35@gmail.com>
2025-02-24 16:18:40 +0100

    Added new connection logic.

--- src/main/java/com/rt_chatApp/services/UserStatusService.java
+++ src/main/java/com/rt_chatApp/services/UserStatusService.java
@@ -1,31 +1,79 @@
 package com.rt_chatApp.services;
 
+import com.rt_chatApp.Dto.FriendDto;
+import com.rt_chatApp.Exceptions.UserNotFoundException;
+import com.rt_chatApp.security.user.User;
 import com.rt_chatApp.security.user.UserRepository;
+import com.rt_chatApp.security.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static com.rt_chatApp.security.user.Status.OFFLINE;
+import static com.rt_chatApp.security.user.Status.ONLINE;
 
 @Service
 @RequiredArgsConstructor
 public class UserStatusService {
 
     private final UserRepository userRepository;
+    private final UserService userService;
+    private final TransactionTemplate transactionTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    private static final ConcurrentHashMap<Integer, String> onlineUsers = new ConcurrentHashMap<>();
+    @Async
+    public CompletableFuture<Void> userOnline(Integer id) {
+        transactionTemplate.execute(status -> {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new UserNotFoundException("User not found when setting it's status to online."));
+            user.setStatus(ONLINE);
+            userRepository.save(user);
 
-    public void addUser(Integer id){
-        userRepository.findById(id).ifPresent(user -> onlineUsers.put(user.getId(), user.getDisplayName()));
-        var users = onlineUsers;
+           return null;
+        });
+
+        return CompletableFuture.completedFuture(null);
     }
 
-    public void removeUser(Integer id) {
-        userRepository.findById(id).ifPresent(user -> onlineUsers.remove(user.getId()));
-        var users = onlineUsers;
+    @Async
+    public CompletableFuture<Void> userOffline(Integer id) {
+        transactionTemplate.execute(status -> {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new UserNotFoundException("User not found when setting it's status to offline."));
+            user.setStatus(OFFLINE);
+            userRepository.save(user);
+
+            return null;
+        });
+
+        return CompletableFuture.completedFuture(null);
     }
 
-    public ConcurrentHashMap<Integer, String> getOnlineUsers() {
-        return onlineUsers;
+    public CompletableFuture<Void> notifyFriends(Integer id){
+        transactionTemplate.execute(status -> {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new UserNotFoundException("User not found when sending status change message."));
+            List<User> friends = user.getFriends();
+            for (User friend : friends) {
+                messagingTemplate.convertAndSendToUser(
+                        String.valueOf(friend.getId()),
+                        "/queue/friends",
+                        FriendDto.builder()
+                                .userId(friend.getId())
+                                .displayName(friend.getDisplayName())
+                                .status(friend.getStatus())
+                                .build());
+            }
+
+            return null;
+        });
+
+        return CompletableFuture.completedFuture(null);
     }
 
 }
