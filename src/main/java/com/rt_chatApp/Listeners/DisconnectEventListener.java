ffdd8f8c158422884438cd2404019f9e085e0075
tpoda <tpoda35@gmail.com>
2025-02-24 16:16:10 +0100

    Modified statuservice for the new friend system.

--- src/main/java/com/rt_chatApp/Listeners/DisconnectEventListener.java
+++ src/main/java/com/rt_chatApp/Listeners/DisconnectEventListener.java
@@ -1,26 +1,20 @@
 package com.rt_chatApp.Listeners;
 
 import com.rt_chatApp.security.user.User;
-import com.rt_chatApp.security.user.UserService;
 import com.rt_chatApp.services.UserStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.security.Principal;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
 public class DisconnectEventListener implements ApplicationListener<SessionDisconnectEvent> {
 
-    private final UserService userService;
     private final UserStatusService statusService;
-    private final SimpMessagingTemplate messagingTemplate;
 
     @Override
     public void onApplicationEvent(SessionDisconnectEvent event) {
@@ -30,12 +24,8 @@ public class DisconnectEventListener implements ApplicationListener<SessionDisco
             Object principalObj = authenticationToken.getPrincipal();
 
             if (principalObj instanceof User user) {
-                statusService.removeUser(user.getId());
-                userService.disconnect(user.getId());
-
-                CompletableFuture.delayedExecutor(500, TimeUnit.MILLISECONDS).execute(() -> {
-                    messagingTemplate.convertAndSend("/topic/onlineUsers", statusService.getOnlineUsers());
-                });
+                statusService.userOffline(user.getId())
+                        .thenCompose(aVoid -> statusService.notifyFriends(user.getId()));
             }
         }
     }
