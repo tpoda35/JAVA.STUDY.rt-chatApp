5e49a2bb6d26ef2f7d7334d9941bf632252f5e8c
tpoda <tpoda35@gmail.com>
2025-02-24 18:11:02 +0100

    Removed unused imports.

--- src/main/java/com/rt_chatApp/controller/FriendController.java
+++ src/main/java/com/rt_chatApp/controller/FriendController.java
@@ -7,7 +7,6 @@ import com.rt_chatApp.services.FriendService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
