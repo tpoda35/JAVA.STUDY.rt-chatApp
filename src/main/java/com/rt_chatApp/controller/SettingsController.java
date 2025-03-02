facd62f293be494c9e3e54ae21e23090c96d4630
tpoda <tpoda35@gmail.com>
2025-03-02 20:42:54 +0100

    Implemented changeDisplayName endpoint.

--- src/main/java/com/rt_chatApp/controller/SettingsController.java
+++ src/main/java/com/rt_chatApp/controller/SettingsController.java
@@ -1,5 +1,6 @@
 package com.rt_chatApp.controller;
 
+import com.rt_chatApp.services.SettingsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/settings")
+@RequestMapping("/api/v1/settings")
 @RequiredArgsConstructor
 public class SettingsController {
+
+    private final SettingsService settingsService;
 
     @PostMapping("/changeDisplayName")
     public void changeDisplayName(
             @RequestParam String newDisplayName
     ) {
-
+        settingsService.changeDisplayName(newDisplayName);
     }
-
 }
