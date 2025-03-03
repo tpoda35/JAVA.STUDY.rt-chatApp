53588e5375040c6194f9cab2ae66038f49a69029
tpoda35 <tpoda35@gmail.com>
2025-03-03 18:56:07 +0100

    Initial commit

---
+++ src/main/java/com/rt_chatApp/Exceptions/IconColorCooldownException.java
@@ -0,0 +1,7 @@
+package com.rt_chatApp.Exceptions;
+
+public class IconColorCooldownException extends RuntimeException{
+    public IconColorCooldownException(String message) {
+        super(message);
+    }
+}
